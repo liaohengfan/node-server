4e5b123dd775cbb865a29343fb4231dc0aa2a867
liaohengfan <liaohengfan@yeah.net>
2018-04-09 17:12:13 +0800

    node server init

--- node-server.js
+++ node-server.js
@@ -21,7 +21,19 @@ http.createServer(function (req, res) {
 
     if (pathname != '/favicon.ico') {  /*过滤请求favicon.ico*/
         console.log(pathname);
-//文件操作获取 static下面的index.html
+        //文件操作获取 static下面的index.html
+        fs.readFile('webs' + pathname,function(err,data){
+
+            if(err){  /*没有这个文件*/
+                console.log('404');
+                res.end(); /*结束响应*/
+            }else{ /*返回这个文件*/
+                res.writeHead(200,{"Content-Type":"text/html;charset='utf-8'"});
+                res.write(data);
+                res.end(); /*结束响应*/
+            }
+
+        })
     }
 
-}).listen(80);
+}).listen(8080);
